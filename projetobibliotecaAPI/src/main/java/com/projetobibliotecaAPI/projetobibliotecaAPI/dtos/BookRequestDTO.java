ca7cb55a4572413619b1ff1499739153d0c7e4ba
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-09-01 11:38:57 -0300

    adding books dtos

---
+++ projetobibliotecaAPI/src/main/java/com/projetobibliotecaAPI/projetobibliotecaAPI/dtos/BookRequestDTO.java
@@ -0,0 +1,31 @@
+package com.projetobibliotecaAPI.projetobibliotecaAPI.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.ISBN;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+public class BookRequestDTO {
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String genero;
+
+    @NotBlank
+    @ISBN(type = org.hibernate.validator.constraints.ISBN.Type.ANY)
+    private String ISBN;
+
+    @Past
+    @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate realeseDate;
+}
