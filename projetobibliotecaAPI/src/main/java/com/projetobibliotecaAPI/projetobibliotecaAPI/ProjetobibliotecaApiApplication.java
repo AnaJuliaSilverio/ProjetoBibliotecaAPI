a82075ad530b1d9f91d73bb2bc69a12a93ecc6e8
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-09-01 11:00:33 -0300

    project structure

---
+++ projetobibliotecaAPI/src/main/java/com/projetobibliotecaAPI/projetobibliotecaAPI/ProjetobibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.projetobibliotecaAPI.projetobibliotecaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobibliotecaApiApplication.class, args);
+	}
+
+}
---
+++ projetobibliotecaAPI/src/test/java/com/projetobibliotecaAPI/projetobibliotecaAPI/ProjetobibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetobibliotecaAPI.projetobibliotecaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
