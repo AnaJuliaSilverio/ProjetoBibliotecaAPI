04c22e3e2c760bd25e9b807e5f7a208736da54f9
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-09-02 13:46:21 -0300

    adding exception support

---
+++ projetobibliotecaAPI/src/main/java/com/projetobibliotecaAPI/projetobibliotecaAPI/expectionHandler/ControllerExceptionHandler.java
@@ -0,0 +1,2 @@
+package com.projetobibliotecaAPI.projetobibliotecaAPI.expectionHandler;public class ControllerExceptionHandler {
+}
---
+++ projetobibliotecaAPI/src/main/java/com/projetobibliotecaAPI/projetobibliotecaAPI/models/BookUnavailableException.java
@@ -0,0 +1,2 @@
+package com.projetobibliotecaAPI.projetobibliotecaAPI.models;public class BookUnavailableException {
+}
